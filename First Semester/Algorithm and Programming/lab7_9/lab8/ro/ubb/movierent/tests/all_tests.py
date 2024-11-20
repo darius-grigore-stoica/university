3823ba8bf3463b1d6557c1c4a8059358a9963148
Darius Grigore Stoica <dariusgrigorestoica@gmail.com>
2024-11-20 18:06:14 +0200

    some refactoring

--- First Semester/Algorithm and Programming/lab7_9/lab8/ro/ubb/movierent/tests/all_tests.py
+++
@@ -1,9 +0,0 @@
-from ro.ubb.movierent.tests.client_test import *
-from ro.ubb.movierent.tests.movie_test import *
-
-def TestAll():
-    test_client_getteri()
-    test_movie_getteri()
-    test_client_setteri()
-    test_movie_setteri()
-    test_add()
